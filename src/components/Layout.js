3774479fc6d82aad588ad068deefdda2f1fe64dc
josiahprathaban <josiah.prathaban@gmail.com>
2021-06-17 10:54:14 +0530

    improve design

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,10 +2,16 @@ import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import "../styles/global.scss"
+import { Helmet } from "react-helmet"
 
 export default function Layout({ children }) {
     return (
         <div className="layout">
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Josiah Prathaban</title>
+                <link rel="icon" href="/favicon.svg" />
+            </Helmet>
             <Navbar/>
             <div className="content">
                 { children }
